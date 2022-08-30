d528d7df3b55733d62d34d16a2a7ac7f0bfde7ea
yrl <1543785919@qq.com>
2022-08-30 21:18:57 +0800

    测试文件003

--- src/main/java/com/add/yrlTest01.java
+++ src/main/java/com/add/yrlTest01.java
@@ -2,8 +2,6 @@ package com.add;
 
 public class yrlTest01 {
     public static void main(String[] args) {
-        System.out.println("测试用");
-        System.out.println("对对对对");
-        System.out.println("大大方方发");
+
     }
 }
