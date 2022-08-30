03979ac0991171051460817e9a49ec1f5e51e038
yrl <1543785919@qq.com>
2022-08-30 20:43:00 +0800

    测试文件003

--- src/main/java/com/add/yrlTest01.java
+++ src/main/java/com/add/yrlTest01.java
@@ -4,5 +4,6 @@ public class yrlTest01 {
     public static void main(String[] args) {
         System.out.println("测试用");
         System.out.println("对对对对");
+        System.out.println("大大方方发");
     }
 }
